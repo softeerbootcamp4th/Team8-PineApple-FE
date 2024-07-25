b7c4a3f1113c886ddf872f8a90fc918b13d2f160
subsub-e <ab13696802@gmail.come>
2024-07-25 18:05:27 +0900

    [feat] eventIntroNav layout 및 기능 구현

--- src/pages/eventIntro/eventIntroMain.jsx
+++ src/pages/eventIntro/eventIntroMain.jsx
@@ -15,17 +15,17 @@ function EventIntroMain() {
   };
 
   return (
-    <div className="bg-gradient-violetblue-cobaltblue w-screen h-screen pt-[150px] flex justify-between">
-      <div className="pl-3000">
-        <div className="flex w-[73%] h-[8%] items-center justify-center bg-primary-berrypurple text-detail-1-bold text-neutral-white mb-600">
+    <div className="bg-gradient-violetblue-cobaltblue h-[872px] pt-[150px] flex justify-between">
+      <div className="ml-3000">
+        <div className="flex w-[350px] h-[50px] items-center justify-center bg-primary-berrypurple text-detail-1-bold text-neutral-white mb-600 text-nowrap">
           진짜 캐스퍼 EV를 얻을 수 있는 기회
         </div>
-        <div className="text-heading-banner-title text-neutral-black mb-1000">
+        <div className="text-heading-banner-title text-neutral-black mb-1000 text-nowrap">
           캐스퍼 EV와
           <br />
           함께 떠나기
         </div>
-        <p className="text-detail-2-regular text-neutral-black ">
+        <p className="text-detail-2-regular text-neutral-black text-nowrap ">
           매일 이벤트를 수행하고 얻은 아이템으로 캐스퍼와 떠나보아요!
         </p>
         <p className="text-detail-2-regular text-neutral-black ">
@@ -44,13 +44,13 @@ function EventIntroMain() {
         <div className="flex mt-600 gap-300">
           <button
             onClick={handleMoveJoin}
-            className="flex items-center justify-center rounded-full px-800 py-400 bg-neutral-white text-neutral-black text-detail-3-semibold"
+            className="flex items-center justify-center transition-transform rounded-full px-800 py-400 bg-neutral-white text-neutral-black text-detail-3-semibold hover:scale-105"
           >
             이벤트 참여하기
           </button>
           <button
             onClick={handleMovenewCarIntro}
-            className="flex items-center justify-center bg-transparent rounded-full px-800 py-400 text-neutral-black text-detail-3-semibold border-[2px] border-solid border-neutral-black"
+            className="flex items-center justify-center bg-transparent rounded-full px-800 py-400 text-neutral-black text-detail-3-semibold border-[2px] border-solid border-neutral-black hover:scale-105 transition-transform"
           >
             캐스퍼 EV 알아보기
             <img src={arrow} alt="move" />
