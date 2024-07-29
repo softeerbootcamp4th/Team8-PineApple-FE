242f000bcb75ae8eceb1b4f13924448ba8c07025
subsub-e <ab13696802@gmail.come>
2024-07-29 17:04:17 +0900

    [fix] PhoneInputModal 로직 수정

--- src/utils/isValidPhoneNumber.js
+++ src/utils/isValidPhoneNumber.js
@@ -1,4 +1,8 @@
 function isValidPhoneNumber(number) {
+  console.log(number.length);
+  if (number.length < 9 || number.length > 11) {
+    return false;
+  }
   const result = /^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/;
   return result.test(number);
 }
