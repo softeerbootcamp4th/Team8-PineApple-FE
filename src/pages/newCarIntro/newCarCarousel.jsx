618eb00f06a1a1b1bb431b4a752118ccb6e1075e
yoonc01 <yoonc01@hotmail.com>
2024-07-30 16:11:45 +0900

    done

--- src/pages/newCarIntro/newCarCarousel.jsx
+++ src/pages/newCarIntro/newCarCarousel.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import newCarImage1 from '@/assets/images/newCarImage1.svg';
 import arrowLeftCircle from '@/assets/images/arrowLeftCircle.svg';
 import arrowRightCircle from '@/assets/images/arrowRightCircle.svg';
 import '@/styles/newCarCarousel.css';
@@ -37,12 +36,12 @@ function NewCarCarousel() {
 
   return (
     <>
-      <div className="mx-5000 mb-1500">
+      <div className="mx-5000 mb-1100">
         <div className="text-body-3-semibold text-primary-blue">Highlights</div>
         <div className="text-heading-2-bold">전력을 다해, CASPER Electric</div>
       </div>
       <div className="mt-1500 mb-1200" id="micro-slider">
-        <div className="micro-slider px-500" ref={sliderRef}>
+        <div className="micro-slider px-1000" ref={sliderRef}>
           <div className="slider-item s1"></div>
           <div className="slider-item s2"></div>
           <div className="slider-item s3"></div>
@@ -50,7 +49,7 @@ function NewCarCarousel() {
           <div className="slider-item s5"></div>
         </div>
       </div>
-      <div className="flex justify-center mt-1500 mb-3000 gap-600">
+      <div className="flex justify-center mt-1200 mb-3000 gap-600">
         <img
           src={arrowLeftCircle}
           alt="leftButton"
