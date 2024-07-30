df564fc78cbcc077118d5feee7bd22201d6e8d50
yoonc01 <yoonc01@hotmail.com>
2024-07-30 15:17:20 +0900

    add margin and padding

--- src/pages/newCarIntro/newCarCarousel.jsx
+++ src/pages/newCarIntro/newCarCarousel.jsx
@@ -26,25 +26,19 @@ function NewCarCarousel() {
       <div className="mx-5000 mb-1500">
         <div className="text-body-3-semibold text-primary-blue">Highlights</div>
         <div className="text-heading-2-bold">전력을 다해, CASPER Electric</div>
-        <div className="mt-1500 mb-1200" id="micro-slider">
-          <div className="micro-slider" ref={sliderRef}>
-            <div className="slider-item s1">1</div>
-            <div className="slider-item s2">2</div>
-            <div className="slider-item s3">3</div>
-            <div className="slider-item s4">4</div>
-            <div className="slider-item s5">5</div>
-            <div className="slider-item s6">6</div>
-          </div>
-          <img
-            src={newCarImage1}
-            alt="newCarImage1"
-            className="w-[1240px] h-[567px]"
-          />
+      </div>
+      <div className="mt-1500 mb-1200" id="micro-slider">
+        <div className="micro-slider px-500" ref={sliderRef}>
+          <div className="slider-item s1"></div>
+          <div className="slider-item s2"></div>
+          <div className="slider-item s3"></div>
+          <div className="slider-item s4"></div>
+          <div className="slider-item s5"></div>
         </div>
-        <div className="flex justify-center gap-600">
-          <img src={arrowLeftCircle} className="hover:cursor-pointer"></img>
-          <img src={arrowRightCircle} className="hover:cursor-pointer"></img>
-        </div>
+      </div>
+      <div className="flex justify-center mt-1500 mb-3000 gap-600">
+        <img src={arrowLeftCircle} className="hover:cursor-pointer"></img>
+        <img src={arrowRightCircle} className="hover:cursor-pointer"></img>
       </div>
     </>
   );
