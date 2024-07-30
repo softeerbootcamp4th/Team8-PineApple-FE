57c5de5bb41be5a7ba2af6a5e203884cd881f6f9
yoonc01 <yoonc01@hotmail.com>
2024-07-30 14:47:25 +0900

    get src from url and animation have to change with package

--- src/pages/newCarIntro/newCarCarousel.jsx
+++ src/pages/newCarIntro/newCarCarousel.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import newCarImage1 from '@/assets/images/newCarImage1.svg';
 import arrowLeftCircle from '@/assets/images/arrowLeftCircle.svg';
 import arrowRightCircle from '@/assets/images/arrowRightCircle.svg';
 
 function NewCarCarousel() {
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://cdn.jsdelivr.net/npm/micro-slider@1.0.9/dist/micro-slider.min.js';
+    script.onload = () => {
+      const __ms = sliderRef.current;
+      const __msSlider = new MicroSlider(__ms, {
+        indicators: true,
+        indicatorText: '',
+      });
+    };
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <div className="mx-5000 mb-1500">
         <div className="text-body-3-semibold text-primary-blue">Highlights</div>
         <div className="text-heading-2-bold">전력을 다해, CASPER Electric</div>
-        <div className="mt-1500 mb-1200">
+        <div className="mt-1500 mb-1200" id="micro-slider">
+          <div className="micro-slider" ref={sliderRef}>
+            <div className="slider-item s1">1</div>
+            <div className="slider-item s2">2</div>
+            <div className="slider-item s3">3</div>
+            <div className="slider-item s4">4</div>
+            <div className="slider-item s5">5</div>
+            <div className="slider-item s6">6</div>
+          </div>
           <img
             src={newCarImage1}
             alt="newCarImage1"
