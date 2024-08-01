a506c0f10335b8d8ac0b6903c5d38545f8e9f461
subsub-e <ab13696802@gmail.come>
2024-08-01 10:11:48 +0900

    [fix] date formatting 수정

--- src/utils/dateFormatting.js
+++ src/utils/dateFormatting.js
@@ -2,7 +2,7 @@ function dateFormatting(date = new Date()) {
   const year = String(date.getFullYear());
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const nowDay = String(date.getDate()).padStart(2, '0');
-  return `${year}.${month}.${nowDay}`;
+  return `${year}-${month}-${nowDay}`;
 }
 
 export default dateFormatting;
