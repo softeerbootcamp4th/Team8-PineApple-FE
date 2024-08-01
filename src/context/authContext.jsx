7b659ae1c76bb382b42f89fb9c8a4b8f6982b416
subsub-e <ab13696802@gmail.come>
2024-08-02 00:17:35 +0900

    [feat] AuthContext 휴대폰 번호에서 유저 객체로 수정

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,14 +1,21 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    const storedUserInfo = localStorage.getItem('userInfo');
+    if (storedUserInfo) {
+      setUserInfo(JSON.parse(storedUserInfo));
+    }
+  }, []);
 
   return (
     //value 묶는 것을 []로 바꿈!
-    <AuthContext.Provider value={{ phoneNumber, setPhoneNumber }}>
+    <AuthContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </AuthContext.Provider>
   );
