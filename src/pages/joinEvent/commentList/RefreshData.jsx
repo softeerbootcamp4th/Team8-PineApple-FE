041c9d6fb529916354b961b6e95f37f43cb88254
subsub-e <ab13696802@gmail.come>
2024-08-01 16:41:44 +0900

    [feat] refresh 버튼 클릭시 rerender 시키는 로직 추가

--- src/pages/joinEvent/commentList/RefreshData.jsx
+++ src/pages/joinEvent/commentList/RefreshData.jsx
@@ -3,14 +3,14 @@ import ReloadComment from '@/assets/icons/reloadComment.svg';
 import PropTypes from 'prop-types';
 import getNowTime from '@/utils/getNowTime';
 
-function RefreshData({ nowTime, setNowTime }) {
+function RefreshData({ nowTime, setNowTime, onClickrefreshData }) {
   return (
     <div className="flex items-center mr-600 gap-400">
       <span className="text-detail-3-regular text-neutral-500">{nowTime}</span>
       <img
         src={ReloadComment}
         alt="ReloadComment"
-        onClick={() => setNowTime(getNowTime())}
+        onClick={onClickrefreshData}
         className="cursor-pointer"
       />
     </div>
@@ -20,6 +20,7 @@ function RefreshData({ nowTime, setNowTime }) {
 RefreshData.propTypes = {
   nowTime: PropTypes.string.isRequired,
   setNowTime: PropTypes.func.isRequired,
+  onClickrefreshData: PropTypes.func.isRequired,
 };
 
 export default RefreshData;
