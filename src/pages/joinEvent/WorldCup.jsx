2a0a2f8c85e483d9f2aa762cff8d2ae6588912e8
yoonc01 <yoonc01@hotmail.com>
2024-08-01 10:49:34 +0900

    add Images in WorldCup

--- src/pages/joinEvent/WorldCup.jsx
+++ src/pages/joinEvent/WorldCup.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import worldCupIntro1 from '@/assets/images/worldCupIntro1.svg';
+import worldCupIntro2 from '@/assets/images/worldCupIntro2.svg';
+import worldCupIntro3 from '@/assets/images/worldCupIntro3.svg';
 
 function WorldCup() {
   return (
-    <div className="">
-      <div className="">
-        <img src={worldCupIntro1}></img>
+    <div className="px-2500">
+      <div className="flex">
+        <div className="h-[610px]"></div>
+        <div className="relative">
+          <img className="absolute " src={worldCupIntro1}></img>
+          <img className="absolute" src={worldCupIntro2}></img>
+          <img className="absolute" src={worldCupIntro3}></img>
+        </div>
       </div>
       <div className=""></div>
     </div>
