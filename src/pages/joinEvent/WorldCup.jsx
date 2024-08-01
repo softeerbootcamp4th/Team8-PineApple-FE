c1d47912df6317264d91f4be60d4dfb55b94085c
yoonc01 <yoonc01@hotmail.com>
2024-08-01 10:43:16 +0900

    made WorldCup component and add in JoinEventIntro file

---
+++ src/pages/joinEvent/WorldCup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import worldCupIntro1 from '@/assets/images/worldCupIntro1.svg';
+
+function WorldCup() {
+  return (
+    <div className="">
+      <div className="">
+        <img src={worldCupIntro1}></img>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default WorldCup;
