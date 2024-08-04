5f4d52443a686578d3d12e417631c18d04b77d4a
subsub-e <ab13696802@gmail.com>
2024-08-05 01:05:28 +0900

    [feat] scroll custom hook

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function useScroll() {
+  const location = useLocation();
+  const mainRef = useRef(null);
+  const worldCupRef = useRef(null);
+  const miniQuizRef = useRef(null);
+  const dailyCommentRef = useRef(null);
+
+  useEffect(() => {
+    const { state } = location;
+    const section = state?.scrollTo;
+
+    switch (section) {
+      case 'main':
+        mainRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'worldCup':
+        worldCupRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'miniQuiz':
+        miniQuizRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'dailyComment':
+        dailyCommentRef.current?.scrollIntoView({ behavior: 'smooth' });
+        break;
+      default:
+        break;
+    }
+  }, [location]);
+
+  return {
+    refs: {
+      mainRef,
+      worldCupRef,
+      miniQuizRef,
+      dailyCommentRef,
+    },
+  };
+}
+
+export default useScroll;
