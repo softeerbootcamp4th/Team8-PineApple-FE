f849293696ad9c11308fcd811b4ffc2313a194e2
yoonc01 <yoonc01@hotmail.com>
2024-08-05 18:07:26 +0900

    same with previous commit

--- src/components/buttons/BluePurpleButton.jsx
+++ src/components/buttons/BluePurpleButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '@/styles/global';
+import '@/styles/global.css';
 
 function BluePurpleButton({ value, onClickFunc, styles, disabled = false }) {
   return (
