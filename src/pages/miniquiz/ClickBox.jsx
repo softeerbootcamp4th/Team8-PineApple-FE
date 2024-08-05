673766a2b596f3ff3b3cf1d9e3fd94b5c31e7bdd
yoonc01 <yoonc01@hotmail.com>
2024-08-05 18:28:26 +0900

    commit for push

--- src/pages/miniquiz/ClickBox.jsx
+++ src/pages/miniquiz/ClickBox.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '@/styles/global.css';
 
 function ClickBox({ text }) {
+  const [isChosen, setIsChosen] = useState(false);
+
   return (
-    <div className="cursor-pointer hover:scale-105 transition-transform set-center text-body-3-semibold bg-neutral-white min-w-[586px] min-h-[120px] rounded-[15px] border-op-30-blue border-2">
+    <button className="hover:scale-105 transition-transform set-center text-body-3-semibold bg-neutral-white min-w-[586px] min-h-[120px] rounded-[15px] border-op-30-blue border-2">
       {text}
-    </div>
+    </button>
   );
 }
 
