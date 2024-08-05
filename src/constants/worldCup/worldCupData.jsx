710e30906254ebe8ba77716b74770d92bd130a50
subsub-e <ab13696802@gmail.come>
2024-08-05 18:56:00 +0900

    [add] worldCupData 추가

---
+++ src/constants/worldCup/worldCupData.jsx
@@ -0,0 +1,71 @@
+import HoverImage1 from '@/assets/images/worldCupImage/hoverImage1.svg';
+import HoverImage2 from '@/assets/images/worldCupImage/hoverImage2.svg';
+import HoverImage3 from '@/assets/images/worldCupImage/hoverImage3.svg';
+import HoverImage4 from '@/assets/images/worldCupImage/hoverImage4.svg';
+import HoverImage5 from '@/assets/images/worldCupImage/hoverImage5.svg';
+import HoverImage6 from '@/assets/images/worldCupImage/hoverImage6.svg';
+import NonHoverImage1 from '@/assets/images/worldCupImage/nonHoverImage1.svg';
+import NonHoverImage2 from '@/assets/images/worldCupImage/nonHoverImage2.svg';
+import NonHoverImage3 from '@/assets/images/worldCupImage/nonHoverImage3.svg';
+import NonHoverImage4 from '@/assets/images/worldCupImage/nonHoverImage4.svg';
+import NonHoverImage5 from '@/assets/images/worldCupImage/nonHoverImage5.svg';
+import NonHoverImage6 from '@/assets/images/worldCupImage/nonHoverImage6.svg';
+import ResultImage1 from '@/assets/images/worldCupImage/resultImage1.svg';
+import ResultImage2 from '@/assets/images/worldCupImage/resultImage2.svg';
+import ResultImage3 from '@/assets/images/worldCupImage/resultImage3.svg';
+import ResultImage4 from '@/assets/images/worldCupImage/resultImage4.svg';
+import ResultImage5 from '@/assets/images/worldCupImage/resultImage5.svg';
+import ResultImage6 from '@/assets/images/worldCupImage/resultImage6.svg';
+
+const worldCupData = [
+  {
+    id: 1,
+    story: '까먹고 차 키를 안 가져왔네요.. 어쩌죠..',
+    solution: '스마트폰, 스마트워치를 활용해 쉽게 열 수 있어요!',
+    image: HoverImage1,
+    grayImage: NonHoverImage1,
+    resultImage: ResultImage1,
+  },
+  {
+    id: 2,
+    story: '배터리가 없는지 차가 멈췄어요!',
+    solution: '캐스퍼 EV는 1회 충전 시 315km를 달릴 수 있답니다!',
+    image: HoverImage2,
+    grayImage: NonHoverImage2,
+    resultImage: ResultImage2,
+  },
+  {
+    id: 3,
+    story: '좁은 골목에서 보행자가 앞을 비켜주지 않아요!',
+    solution: '가상 엔진 사운드로 보행자들에게 알릴 수 있어요!',
+    image: HoverImage3,
+    grayImage: NonHoverImage3,
+    resultImage: ResultImage3,
+  },
+  {
+    id: 4,
+    story: '차를 빼야 하는데 주변에 차량이 너무 많아요',
+    solution: '서라운드뷰 모니터를 통해 차량 주변을 확인할 수 있어요!',
+    image: HoverImage4,
+    grayImage: NonHoverImage4,
+    resultImage: ResultImage4,
+  },
+  {
+    id: 5,
+    story: '고속도로인데 갑자기 잠이 쏟아져요!',
+    solution: '잠시 멈추고, 길어진 휠 베이스로 넓은 2열 공간에서 쉬다 가세요!',
+    image: HoverImage5,
+    grayImage: NonHoverImage5,
+    resultImage: ResultImage5,
+  },
+  {
+    id: 6,
+    story: '쌩쌩 달리는 뒷차 때문에 차선을 변경하기 어려워요!',
+    solution: '후측방 모니터를 통해 뒤에서 오는 차량을 확인할 수 있어요!',
+    image: HoverImage6,
+    grayImage: NonHoverImage6,
+    resultImage: ResultImage6,
+  },
+];
+
+export default worldCupData;
