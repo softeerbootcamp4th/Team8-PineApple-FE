6d5f32e06f1f0488e6c9ad22284cf3e61b224d0c
yoonc01 <yoonc01@hotmail.com>
2024-08-05 18:08:20 +0900

    also same with previous commit

--- src/pages/miniquiz/ClickBox.jsx
+++ src/pages/miniquiz/ClickBox.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '@/styles/global.css';
 
 function ClickBox({ text }) {
   return (
-    <div className="flex justify-center items-center bg-neutral-white min-w-[586px] min-h-[120px] rounded-[15px] border-op-30-blue border-2">
+    <div className="set-center bg-neutral-white min-w-[586px] min-h-[120px] rounded-[15px] border-op-30-blue border-2">
       {text}
     </div>
   );
