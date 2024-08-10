cca72300a564d6802b79604bda5ae6e6e37dd940
yoonc01 <yoonc01@hotmail.com>
2024-08-11 01:17:32 +0900

    add BlueButton disabled property

--- src/components/buttons/BlueButton.jsx
+++ src/components/buttons/BlueButton.jsx
@@ -5,9 +5,10 @@ import '@/styles/global.css';
 function BlueButton({ value, styles, onClickFunc, disabled = false }) {
   return (
     <button
-      onClick={disabled === false ? onClickFunc : undefined}
+      onClick={onClickFunc}
       // text-detail and padding have to be fixed, 인자로 추가할까??
       className={`${styles} ${disabled ? 'opacity-30' : 'opacity-100 hover-scale-ani'} set-center rounded-full bg-primary-blue text-neutral-white`}
+      disabled={disabled}
     >
       {value}
     </button>
