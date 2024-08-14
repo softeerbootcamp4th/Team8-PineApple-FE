184a2672d402947bd80b084dcf62bc2aad5efafa
yoonc01 <yoonc01@hotmail.com>
2024-08-14 17:56:58 +0900

    add AdminEventStatus page but not done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AdminEventStatus from './pages/AdminEventStatus/AdminEventStatus';
+import AdminEventStatus from '@/pages/AdminEventStatus/AdminEventStatus';
 
 function App() {
   return (
