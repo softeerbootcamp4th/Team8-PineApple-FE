223bf7c41e0c8d6f0de88c3313d174ad0c103371
yoonc01 <yoonc01@hotmail.com>
2024-08-15 20:44:15 +0900

    edit tabHeader

--- admin/src/components/header/TabHeader.jsx
+++ admin/src/components/header/TabHeader.jsx
@@ -1,57 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import NavLinkItem from '@/components/header/NavLinkItem';
 
 function TabHeader() {
   return (
     <div className="pt-1000 flex justify-start items-center gap-500 w-[90%]">
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          `${
-            isActive
-              ? 'text-body-3-bold text-neutral-black border-b-black border-b-solid border-b-[3px]'
-              : 'text-detail-1-regular text-neutral-500 hover:scale-110 transition-transform duration-300'
-          }`
-        }
-      >
-        미니퀴즈 질문
-      </NavLink>
-      <NavLink
-        to="/miniQuizAnswer"
-        className={({ isActive }) =>
-          `${
-            isActive
-              ? 'text-body-3-bold text-neutral-black border-b-black border-b-solid border-b-[3px]'
-              : 'text-detail-1-regular text-neutral-500 hover:scale-110 transition-transform duration-300'
-          }`
-        }
-      >
-        미니퀴즈 답변
-      </NavLink>
-      <NavLink
-        to="/draw"
-        className={({ isActive }) =>
-          `${
-            isActive
-              ? 'text-body-3-bold text-neutral-black border-b-black border-b-solid border-b-[3px]'
-              : 'text-detail-1-regular text-neutral-500 hover:scale-110 transition-transform duration-300'
-          }`
-        }
-      >
-        응모 결과
-      </NavLink>
-      <NavLink
-        to="/reward"
-        className={({ isActive }) =>
-          `${
-            isActive
-              ? 'text-body-3-bold text-neutral-black border-b-black border-b-solid border-b-[3px]'
-              : 'text-detail-1-regular text-neutral-500 hover:scale-110 transition-transform duration-300'
-          }`
-        }
-      >
-        상품 목록
-      </NavLink>
+      <NavLinkItem path="/" value="미니퀴즈 질문" />
+      <NavLinkItem path="/miniQuizAnswer" value="미니퀴즈 답변" />
+      <NavLinkItem path="/draw" value="응모 결과" />
+      <NavLinkItem path="/reward" value="상품 목록" />
+      <NavLinkItem path="/adminEventStatus" value="이벤트 현황" />
+      <NavLinkItem path="/uploadReward" value="선착순 업로드" />
     </div>
   );
 }
