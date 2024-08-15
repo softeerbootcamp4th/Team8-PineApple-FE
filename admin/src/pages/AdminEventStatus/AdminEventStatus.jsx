2207a7762c9502eae28b509d8686e52de870e0ce
yoonc01 <yoonc01@hotmail.com>
2024-08-15 22:21:20 +0900

    edit AdminEventStatus

--- admin/src/pages/AdminEventStatus/AdminEventStatus.jsx
+++ admin/src/pages/AdminEventStatus/AdminEventStatus.jsx
@@ -4,8 +4,9 @@ import PrizeTable from '@/pages/AdminEventStatus/PrizeTable';
 
 function AdminEventStatus() {
   return (
-    <div className="px-[5%] flex flex-col text-nowrap w-screen">
+    <div className="mt-10 px-[5%] flex flex-col text-nowrap w-screen">
       <PrizeTable />
+      <div className="mt-10 border-[1px] border-[#b5b3b3]"></div>
       <EntryTable />
     </div>
   );
