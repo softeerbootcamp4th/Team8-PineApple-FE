f4bb76ecf4db396d74609687d05cd4ae07bb1cfd
subsub-e <ab13696802@gmail.com>
2024-08-15 15:17:59 +0900

    [fix] router setting

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
-import AdminEventStatus from '@/pages/AdminEventStatus/AdminEventStatus';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import AdminHeader from '@/components/header/AdminHeader';
+import { DateProvider } from '@/context/dateContext';
 
 function App() {
   return (
-    <>
-      <AdminEventStatus />
-    </>
+    <DateProvider>
+      <div className="bg-[#DADADA] flex flex-col items-center min-w-[700px]">
+        <AdminHeader />
+        <div className="w-[90%] flex flex-col items-center pb-2000">
+          <Outlet />
+        </div>
+      </div>
+    </DateProvider>
   );
 }
 
