978b4d057fa1006c6ea6a1af807eee45e7202cfd
subsub-e <ab13696802@gmail.come>
2024-08-16 16:37:44 +0900

    [feat] admin 권한 없을때 ErrorPage 라우팅

---
+++ admin/src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div className="w-screen h-screen bg-white text-heading-banner-title text-neutral-black set-center">
+      접근 권한이 없습니다!!
+    </div>
+  );
+}
+export default ErrorPage;
