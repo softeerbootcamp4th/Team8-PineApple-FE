7b638e898fe09be3f616d9f5f63565d38b581a0b
subsub-e <ab13696802@gmail.come>
2024-08-16 16:34:42 +0900

    [feat] useFetch 훅에서 예외 처리

--- admin/src/hooks/useFetch.js
+++ admin/src/hooks/useFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function useFetch(api, params) {
+  const navigate = useNavigate();
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -8,9 +10,23 @@ function useFetch(api, params) {
   const fetchData = useCallback(async () => {
     try {
       const response = await api(params);
-
       if (typeof response === 'object') {
-        setData(response);
+        if (response.code === 'UNAUTHORIZED') {
+          navigate('/error');
+        } else if (initialData.code === 'NO_QUIZ_INFO') {
+          setQuizData({
+            quizId: '0',
+            quizDescription: '',
+            quizQuestions: {
+              1: '',
+              2: '',
+              3: '',
+              4: '',
+            },
+          });
+        } else {
+          setData(response);
+        }
       } else if (typeof response === 'string') {
         setData({ message: response });
       } else {
