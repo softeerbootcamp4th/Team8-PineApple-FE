d0842702f04b8159c83a80fea9296c0ffc5b2957
subsub-e <ab13696802@gmail.com>
2024-08-18 23:55:46 +0900

    [add] probability page add

--- admin/src/components/header/TabHeader.jsx
+++ admin/src/components/header/TabHeader.jsx
@@ -3,11 +3,12 @@ import NavLinkItem from '@/components/header/NavLinkItem';
 
 function TabHeader() {
   return (
-    <div className="pt-1000 flex justify-start items-center gap-500 w-[90%]">
+    <div className="pt-1000 flex justify-between items-center w-[90%]">
       <NavLinkItem path="/" value="미니퀴즈 질문" />
       <NavLinkItem path="/miniQuizAnswer" value="미니퀴즈 답변" />
       <NavLinkItem path="/draw" value="응모 결과" />
       <NavLinkItem path="/reward" value="상품 목록" />
+      <NavLinkItem path="/probability" value="응모 당첨 확률" />
       <NavLinkItem path="/uploadReward" value="선착순 업로드" />
       <NavLinkItem path="/uploadPrize" value="경품 업로드" />
       <NavLinkItem path="/adminEventStatus" value="이벤트 현황" />
