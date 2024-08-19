064655743ea6829eaf7f12db9ba62e058ffec877
subsub-e <ab13696802@gmail.com>
2024-08-19 23:59:40 +0900

    [feat] JoinIntro 페이지 slide-up motion 추가

--- service/src/pages/joinEvent/JoinEventIntroMain.jsx
+++ service/src/pages/joinEvent/JoinEventIntroMain.jsx
@@ -7,6 +7,8 @@ import BluePurpleButton from '@/components/buttons/BluePurpleButton';
 import { postReward } from '@/api/rapple/index';
 import { useNavigate } from 'react-router-dom';
 import { getScenario } from '@/api/scenario';
+import { animationVariants } from '@/styles/FramerMotion';
+import { motion } from 'framer-motion';
 
 function JoinEventIntroMain() {
   const navigate = useNavigate();
@@ -60,47 +62,69 @@ function JoinEventIntroMain() {
       <div className="bg-join-event-main bg-cover bg-center h-screen pt-[250px] flex flex-col">
         <div className="flex gap-2000 px-3000">
           <div className="space-y-1200">
-            <div className="flex items-center gap-300">
-              <CarCard />
-              <div className="text-heading-1-bold text-neutral-white">+</div>
-              <ToolBoxCard />
-            </div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={animationVariants}
+              transition={{ duration: 0.6, ease: 'easeOut' }}
+            >
+              <div className="flex items-center gap-300">
+                <CarCard />
+                <div className="text-heading-1-bold text-neutral-white">+</div>
+                <ToolBoxCard />
+              </div>
+            </motion.div>
             <div
               className={`text-center underline text-neutral-white text-shadow-default ${phoneNumber ? 'invisible' : 'visible'}`}
             >
-              <span className="cursor-pointer" onClick={openPhoneModal}>
-                보유 아이템이 있다면? 전화번호 입력하고 내 아이템 개수 확인하기
-              </span>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={animationVariants}
+                transition={{ duration: 0.5, ease: 'easeOut', delay: 1.0 }}
+              >
+                <span className="cursor-pointer" onClick={openPhoneModal}>
+                  보유 아이템이 있다면? 전화번호 입력하고 내 아이템 개수
+                  확인하기
+                </span>
+              </motion.div>
             </div>
           </div>
-          <div className="relative flex flex-col">
-            <div className="text-heading-banner-title-2 text-nowrap mb-1000">
-              <span className="text-gradient-blue-purple">캐스퍼 EV</span>와
-              떠나기
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={animationVariants}
+            transition={{ duration: 0.6, ease: 'easeOut', delay: 0.5 }}
+          >
+            <div className="relative flex flex-col">
+              <div className="text-heading-banner-title-2 text-nowrap mb-1000">
+                <span className="text-gradient-blue-purple">캐스퍼 EV</span>와
+                떠나기
+              </div>
+              <div className="w-[84px] bg-op-30-blue px-400 py-100 mb-400 text-detail-2-medium text-neutral-white">
+                Day {day}
+              </div>
+              <div className="whitespace-pre-line h-1800 text-detail-1-regular text-neutral-black mb-1500">
+                {scenario}
+              </div>
+              <div>
+                <BluePurpleButton
+                  value="결과 보기"
+                  onClickFunc={handleReward}
+                  styles="text-body-3-regular px-5000 py-400"
+                  disabled={isRewardButtonDisabled}
+                />
+              </div>
+              {isRewardButtonDisabled && (
+                <>
+                  <div className="absolute top-[417px] left-[279px] h-0 w-0 border-x-[12px] border-b-[12px] border-x-transparent border-b-neutral-white"></div>
+                  <div className="absolute top-[423px] left-[91px] w-[400px] rounded-[5px] py-300 text-center bg-neutral-white text-primary-blue">
+                    이벤트에 참여해서 추첨을 위한 아이템을 모아보아요!
+                  </div>
+                </>
+              )}
             </div>
-            <div className="w-[84px] bg-op-30-blue px-400 py-100 mb-400 text-detail-2-medium text-neutral-white">
-              Day {day}
-            </div>
-            <div className="whitespace-pre-line h-1800 text-detail-1-regular text-neutral-black mb-1500">
-              {scenario}
-            </div>
-            <div>
-              <BluePurpleButton
-                value="결과 보기"
-                onClickFunc={handleReward}
-                styles="text-body-3-regular px-5000 py-400"
-                disabled={isRewardButtonDisabled}
-              />
-            </div>
-            {isRewardButtonDisabled && (
-              <>
-                <div className="absolute top-[417px] left-[279px] h-0 w-0 border-x-[12px] border-b-[12px] border-x-transparent border-b-neutral-white"></div>
-                <div className="absolute top-[423px] left-[91px] w-[400px] rounded-[5px] py-300 text-center bg-neutral-white text-primary-blue">
-                  이벤트에 참여해서 추첨을 위한 아이템을 모아보아요!
-                </div>
-              </>
-            )}
-          </div>
+          </motion.div>
         </div>
       </div>
       {openPhoneInputModal && (
