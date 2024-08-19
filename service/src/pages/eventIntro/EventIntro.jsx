1f31be543e6510e5752e5bb2c4459bea3522f3e5
subsub-e <ab13696802@gmail.com>
2024-08-19 23:59:06 +0900

    [feat] EventIntro 페이지 slide-up motion 추가

--- service/src/pages/eventIntro/EventIntro.jsx
+++ service/src/pages/eventIntro/EventIntro.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import EventIntroMain from '@/pages/eventIntro/EventIntroMain';
 import EventIntroNav from '@/pages/eventIntro/EventIntroNav';
 import EventIntroRewards from '@/pages/eventIntro/EventIntroRewards';
+import { animationVariants } from '@/styles/FramerMotion';
+import { motion } from 'framer-motion';
 
 function EventIntro() {
   return (
     <div>
       <EventIntroMain />
       <div className="bg-gradient-lightblue-white-vertical mt-[1px]">
-        <EventIntroNav />
-        <EventIntroRewards />
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={animationVariants}
+          transition={{ duration: 0.6, ease: 'easeOut', delay: 0.5 }}
+        >
+          <EventIntroNav />
+          <EventIntroRewards />
+        </motion.div>
       </div>
     </div>
   );
