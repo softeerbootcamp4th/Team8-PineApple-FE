79d9ca41d5f3d088d138ca27b0ce3142cfa68e21
yoonc01 <yoonc01@hotmail.com>
2024-08-19 16:47:51 +0900

    app 에서 DateContext를 사용하기 위해 main으로 provider 옮김

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '@/styles/global.css';
+import { DateProvider } from '@/context/dateContext';
 import { RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import router from '@/router';
+import '@/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />,
+  <DateProvider>
+    <RouterProvider router={router} />,
+  </DateProvider>,
 );
