0b6b3bf1138197670c7a5c59d7c3c9d0999cdcd7
subsub-e <ab13696802@gmail.come>
2024-08-19 17:08:56 +0900

    [feat] shortDateFormat 로직

---
+++ service/src/utils/shortDateFormat.js
@@ -0,0 +1,13 @@
+function shortDateFormat(dateString) {
+  const date = new Date(dateString);
+
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${month}월${day}일 ${hours}시${minutes}분`;
+}
+
+export default shortDateFormat;
