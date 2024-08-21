49fc3880fecd8191911e699a17f3a3cff58e0af9
yoonc01 <yoonc01@hotmail.com>
2024-08-21 15:12:12 +0900

    change noQuizMain ment

--- service/src/pages/miniquiz/NoQuizMain.jsx
+++ service/src/pages/miniquiz/NoQuizMain.jsx
@@ -1,18 +1,16 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import WhiteButton from '@/components/buttons/WhiteButton';
 import { useNavigate } from 'react-router-dom';
 
 function NoQuizMain() {
-  const navigate = useNavigate(); //useNavigate은 리렌더링 시에는 재생성이 되지 않음 그렇기에 useCallback 안 씀
+  const navigate = useNavigate();
 
-  const handleExit = useCallback(() => {
-    navigate('/event');
-  }, []);
+  const handleExit = () => navigate('/event'); //navigate는 동일한 주소를 가지므로 useCallback을 사용할 이유가 없음
 
   return (
     <div className="flex flex-col items-center h-screen bg-miniquiz-paper pt-5000">
       <div className="text-heading-banner-title-3 text-gradient-blue-purple py-500">
-        퀴즈 정보가 없습니다.
+        12시부터 13까지는 퀴즈 준비시간입니다!!!
       </div>
       <div className="text-heading-banner-title-3 text-gradient-blue-purple pb-1000">
         이벤트 페이지로 돌아가시겠습니까???
