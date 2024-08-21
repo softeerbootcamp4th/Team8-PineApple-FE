2858550f1505438998976309057bcad909eb9006
subsub-e <ab13696802@gmail.come>
2024-08-21 18:38:42 +0900

    [fix] loading 화면 추가

--- service/src/pages/joinEvent/commentList/Redirect.jsx
+++ service/src/pages/joinEvent/commentList/Redirect.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { getRedirectLink } from '@/api/comment/index';
 import { useParams, useNavigate } from 'react-router-dom';
+import Loading from '@/assets/icons/Loading.svg';
 
 function Redirect() {
   const navigate = useNavigate();
@@ -13,6 +14,12 @@ function Redirect() {
   useEffect(() => {
     getLink();
   }, []);
+
+  return (
+    <div className="w-screen h-screen set-center bg-neutral-400">
+      <img src={Loading} alt="Loading" className="rotate-360" />
+    </div>
+  );
 }
 
 export default Redirect;
