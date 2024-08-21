a00f1a8f13331a9320dc1334eea283b04598e055
subsub-e <ab13696802@gmail.come>
2024-08-21 15:09:24 +0900

    [fix] metaTag 수정

--- service/vite.config.js
+++ service/vite.config.js
@@ -2,36 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
-import prerender from '@prerenderer/rollup-plugin';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    prerender({
-      routes: [
-        '/',
-        '/event',
-        '/event/worldCup',
-        '/event/miniQuiz',
-        '/introduce',
-      ],
-      renderer: '@prerenderer/renderer-puppeteer',
-      rendererOptions: {
-        maxConcurrentRoutes: 1,
-        renderAfterTime: 500,
-      },
-      postProcess(renderedRoute) {
-        const apiUrl = process.env.VITE_API_URL;
-        renderedRoute.html = renderedRoute.html
-          .replace(/http:/i, 'https:')
-          .replace(/(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i, apiUrl);
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       { find: '@', replacement: resolve(__dirname, 'src') },
