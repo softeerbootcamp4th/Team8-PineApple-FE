ea967d11d18333f3edd629d03311d3323f78b759
yoonc01 <yoonc01@hotmail.com>
2024-08-21 14:28:44 +0900

    add alt in img tag

--- service/src/pages/newCarIntro/NewCarDetailBox.jsx
+++ service/src/pages/newCarIntro/NewCarDetailBox.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function NewCarDetailBox({ details }) {
-  const { title, description, imageSrc } = details;
+  const { title, description, imageSrc, alt } = details;
   return (
     <div>
-      <img src={imageSrc} className="h-[435px] w-[703px]"></img>
+      <img
+        src={imageSrc}
+        alt={alt}
+        className="h-[435px] w-[703px]"
+        draggable="false"
+      ></img>
       <div className="text-body-1-bold text-neutral-black">{title}</div>
       <div className="text-detail-2-medium text-neutral-black">
         {description}
@@ -18,6 +23,7 @@ NewCarDetailBox.propTypes = {
   details: PropTypes.shape({
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
     imageSrc: PropTypes.string.isRequired,
   }).isRequired,
 };
