19dcbf99b067f46cf6b6bd3b9d25a9c0e59111b9
yoonc01 <yoonc01@hotmail.com>
2024-08-22 16:48:46 +0900

    중복 제거

--- service/src/utils/linkToFunc.js
+++ service/src/utils/linkToFunc.js
@@ -1,7 +1,8 @@
-export const linkToFreeRide = () => {
-  window.open('https://casper.hyundai.com/vehicles/test-driving/intro');
+export const openInNewTab = url => {
+  window.open(url);
 };
 
-export const linkToPreOrder = () => {
-  window.open('https://casper.hyundai.com/vehicles/electric/making/model');
-};
+export const linkToFreeRide = () =>
+  openInNewTab('https://casper.hyundai.com/vehicles/test-driving/intro');
+export const linkToPreOrder = () =>
+  openInNewTab('https://casper.hyundai.com/vehicles/electric/making/model');
