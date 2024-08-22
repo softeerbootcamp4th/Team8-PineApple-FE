8b8542f1b42798c02cd59c58b57c2f36c460f6b7
yoonc01 <yoonc01@hotmail.com>
2024-08-22 20:51:38 +0900

    slideupmotion componenent로 변경

--- service/src/pages/eventIntro/EventIntro.jsx
+++ service/src/pages/eventIntro/EventIntro.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import EventIntroMain from '@/pages/eventIntro/EventIntroMain';
 import EventIntroNav from '@/pages/eventIntro/EventIntroNav';
 import EventIntroRewards from '@/pages/eventIntro/EventIntroRewards';
-import { animationVariants } from '@/styles/FramerMotion';
-import { motion } from 'framer-motion';
+import SlideUpMotion from '@/components/SlideUpMotion/SlideUpMotion';
 import { Helmet } from 'react-helmet-async';
 
 function EventIntro() {
@@ -28,18 +27,9 @@ function EventIntro() {
       <EventIntroMain />
       <div className="bg-gradient-lightblue-white-vertical mt-[1px]">
         <EventIntroNav />
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={animationVariants}
-          transition={{
-            duration: 0.6,
-            ease: 'easeOut',
-            delay: 0.1,
-          }}
-        >
+        <SlideUpMotion delay={0.1}>
           <EventIntroRewards />
-        </motion.div>
+        </SlideUpMotion>
       </div>
     </div>
   );
